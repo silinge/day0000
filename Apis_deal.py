0df63169c26ac2dd13bbd0c083fd36cfce894651
silinge <gatesyyy@gmail.com>
2018-12-15 12:15:21 +0800

    再更新一次，update again

--- Apis_deal.py
+++ Apis_deal.py
@@ -12,12 +12,14 @@
 #这个网站无用了 跳转的链接需要注册获得api 所以就跳过吧。
 #——————————————————————————
 # part2
-import json
-jsonString = '{"arrayOfNums":[{"number":0},{"number":1},{"number":2}],' \
-             '"arrayOfFruits":[{"fruit":"apple"},{"fruit":"banana"},{"fruit":"pear"}]}'
-jsonObj = json.loads(jsonString)
-print(jsonObj.get("arrayOfNums"))
-print(jsonObj.get("arrayOfNums")[1])
-print(jsonObj.get("arrayOfNums")[1].get("number")+
-      jsonObj.get("arrayOfNums")[2].get("number"))
-print(jsonObj.get("arrayOfFruits")[2].get("fruit"))
+# import json
+# jsonString = '{"arrayOfNums":[{"number":0},{"number":1},{"number":2}],' \
+#              '"arrayOfFruits":[{"fruit":"apple"},{"fruit":"banana"},{"fruit":"pear"}]}'
+# jsonObj = json.loads(jsonString)
+# print(jsonObj.get("arrayOfNums"))
+# print(jsonObj.get("arrayOfNums")[1])
+# print(jsonObj.get("arrayOfNums")[1].get("number")+
+#       jsonObj.get("arrayOfNums")[2].get("number"))
+# print(jsonObj.get("arrayOfFruits")[2].get("fruit"))
+#范例 处理json的字段吧
+#——————————————————————————
