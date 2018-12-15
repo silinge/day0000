ba3c83632f3c96c9b6f356f9c02ad17634b3f42a
silinge <gatesyyy@gmail.com>
2018-12-15 16:50:39 +0800

    new branch and update file with src download

--- Apis_deal.py
+++ Apis_deal.py
@@ -69,3 +69,59 @@
 #这一部分是能正常运行运行的了，以后命名变量尽量不要用近似的名称 放错了地方很难发现
 
 #——————————————————————————
+# part3
+# from urllib.request import urlretrieve
+# from urllib.request import  urlopen
+# from bs4 import BeautifulSoup
+#
+# html = urlopen("http://www.pythonscraping.com")
+# bsObj = BeautifulSoup(html, features='lxml')
+# imageLocation = bsObj.find("a", {"id":"logo"}).find("img")["src"]
+# urlretrieve(imageLocation, "logo.jpg")
+#保存这个网页上面的一张图片
+
+#——————————————————————————
+# part 4
+import os
+from urllib.request import urlretrieve
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+downloadDirectory = "download"
+baseUrl = "http://pythonscraping.com"
+
+def getAbsoluteURL(baseUrl, source):
+    if source.startswith("http://www."):
+        url = "http://"+source[11:]
+    elif source.startswith("http://"):
+        url = source
+    elif source.startswith("www."):
+        url = source[4:]
+        url = "http://" + source
+    else:
+        url = baseUrl+"/"+source
+    if baseUrl not in url:
+        return None
+    return url
+
+def getDownloadPath(baseUrl, absoluteUrl, downloadDirectory):
+    path = absoluteUrl.replace("www.", "")
+    path = path.replace(baseUrl, "")
+    path = downloadDirectory + path
+    directory = os.path.dirname(path)
+
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    return path
+
+html = urlopen("http://www.pythonscraping.com")
+bsObj = BeautifulSoup(html, features='lxml')
+downloadList = bsObj.findAll(src = True)
+
+for download in downloadList:
+    fileUrl = getAbsoluteURL(baseUrl, download["src"])
+    if fileUrl is not None:
+        print(fileUrl)
+urlretrieve(fileUrl, getDownloadPath(baseUrl, fileUrl, downloadDirectory))
+#下载网页上所以带有src属性的文件
