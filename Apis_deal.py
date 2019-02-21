493b630d73fda220203db1d2bb3299925e66f579
xuwilliam <gatesyyy@gmail.com>
2019-02-21 21:39:28 +0800

    pull pull

--- Apis_deal.py
+++ Apis_deal.py
@@ -705,9 +705,11 @@
 # bigramsDist = FreqDist(bigrams)
 # print(bigramsDist[("Sir", "Robin")])
 
-from nltk.book import *
-from nltk import ngrams
-fourgrams = ngrams(text6, 4)
-for fourgram in fourgrams:
-    if fourgram[0] == "coconut":
-        print(fourgram)
+# from nltk.book import *
+# # from nltk import ngrams
+# # fourgrams = ngrams(text6, 4)
+# # for fourgram in fourgrams:
+# #     if fourgram[0] == "coconut":
+# #         print(fourgram)
+
+push push
