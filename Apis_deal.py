651c646f593cfb51f3b697baeb69f364253b428f
xuwilliam <gatesyyy@gmail.com>
2019-03-20 11:16:06 +0800

    captcha reading problem

--- Apis_deal.py
+++ Apis_deal.py
@@ -868,3 +868,55 @@ import subprocess
 # cleanFile("C:/Users/XuWilliam/Pictures/Gradiant.tif", "C:/Users/XuWilliam/Pictures/Gradiant_clean2.png")
 # cleanFile(r"‪C:\Users\XuWilliam\Pictures\Gradiant.tif", r"C:\Users\XuWilliam\Pictures\Gradiant_clean2.png")
 # 对tif格式图片不友好 还要注意处理路径问题
+# import requests
+# from requests.auth import AuthBase
+# from requests.auth import HTTPBasicAuth
+#
+# auth = HTTPBasicAuth("William", 'password')
+#
+# r = requests.post(url="http://pythonscraping.com/pages/auth/login.php", auth=auth)
+#
+# print(r.text)
+#破解验证码
+# from urllib.request import urlretrieve
+# from urllib.request import urlopen
+# from bs4 import BeautifulSoup
+# import subprocess
+# import requests
+# from PIL import Image
+# from PIL import ImageOps
+#
+# def cleanImage(imagePath):
+#     image = Image.open(imagePath)
+#     image = image.point(lambda  x: 0 if x < 135 else 255)
+#     borderImage = ImageOps.expand(image, border=20, fill = 'white')
+#     borderImage.save(imagePath)
+#
+# html = urlopen("http://www.pythonscraping.com/humans-only")
+# bsObj = BeautifulSoup(html, features="lxml")
+# imageLocatin = bsObj.find("img", {"title":"Image CAPTCHA"})["src"]
+# formBuildId = bsObj.find("input", {"name":"form_build_id"})["value"]
+# captchaSid = bsObj.find("input", {"name":"captcha_sid"})["value"]
+# captchaToken = bsObj.find("input", {"name":"captcha_token"})["value"]
+#
+# captchaUrl = "http://pythonscraping.com"+imageLocatin
+# urlretrieve(captchaUrl, "captcha.jpg")
+# cleanImage("captcha.jpg")
+# p = subprocess.Popen(["tesseract", "captcha.jpg", "captcha"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+# p.wait()
+# f = open("captcha.txt", "r")
+#
+# captchaResponse = f.read().replace(" ", "").replace("\n", "")
+# print("Captcha solution attempt: "+captchaResponse)
+#
+# if len(captchaResponse) == 5:
+#     params = {"captcha_token":captchaToken, "captcha_sid":captchaSid, "form_id":"comment_node_page_form",
+#               "form_build_id":formBuildId, "captcha_response":captchaResponse, "name":"Ryan Mitchell",
+#               "subject":"I come to seek the Grail", "comment_body[und][0][value]":"...and I am definitely not a bot"}
+#     r = requests.post("http://www.pythonscraping.com/comment/reply/10", data=params)
+#     responseObj = BeautifulSoup(r.text)
+#     if responseObj.find("div", {"class":"messages"}) is not None:
+#         print(responseObj.find("div", {"class":"messages"}).get_text())
+# else:
+#     print("There was a problem reading the CAPTCHA correctly!")
+# #反正运行了几次都失败了
