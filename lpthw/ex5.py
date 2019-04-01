7d523363de8029ec0612862c4eccd2bbcd382622
xuwilliam <gatesyyy@gmail.com>
2019-04-01 11:16:21 +0800

    f'for f_string {V}.'

---
+++ lpthw/ex5.py
@@ -0,0 +1,21 @@
+name = 'Xv Williams'
+age = 30
+height = 178
+weight = height
+my_eyes = "Black"
+my_teeth = "White"
+my_hair = "Black"
+
+print(f"Let's talk about {name}.")
+print(f"He's {height} cm tall.")
+print(f"you can also add f_string like this {age + weight}, that's fine")
+print(f"or this {str(weight) + my_teeth}, make sure these are the some type.")
+# print(f"or put f_string in another f_strings like {f"Hello, {my_age}"}, that's not ok")
+print(f"He's {weight} Jin heavy.")
+print("Is he too heavy?")
+print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+print(f"Hie teeth are usually {my_teeth} depending on the coffee.")
+
+total = age + weight + height
+print(f"If I add {age}, {height}, and {weight} I get {total}")
+#shift + F6 rename refactor all name in once
